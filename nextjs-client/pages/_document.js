465d905bbb8e40992bc74bc8724e438fa3ad6756
Emily <pheepia7788@gmail.com>
2022-09-06 12:25:09 -0700

    add google fonts

---
+++ nextjs-client/pages/_document.js
@@ -0,0 +1,33 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
+            rel="stylesheet"
+          />
+
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Cantarell&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
--- nextjs-client/src/components/Layout/Navbar/style.js
+++ nextjs-client/src/components/Layout/Navbar/style.js
@@ -3,54 +3,50 @@ import styled from '@emotion/styled';
 export const Container = styled.div`
   display: flex;
   justify-content: space-between;
-  height: 7.3rem;
-  background-color: red;
+  height: 7rem;
+  background-color: white;
+  padding: 0 9.4rem;
 
   .left-box {
     display: flex;
     align-items: center;
-    background-color: green;
 
     .logo-wrapper {
-      width: 12rem;
-      height: 4.7rem;
+      width: 11rem;
+      height: 4.6rem;
       position: relative;
       margin: 1rem;
     }
 
     .menu-items-wrapper {
       display: flex;
+      margin: 0 1rem;
+      font-family: 'Cantarell', sans-serif;
     }
   }
 
   .right-box {
-    /* width: 200px;
-    height: 100%; */
-    background-color: green;
     display: flex;
     align-items: center;
   }
 
   .insta-icon-wrapper {
-    background-color: purple;
     display: flex;
     justify-content: center;
     align-items: center;
     width: 5rem;
     height: 5rem;
 
-    font-size: 2.4rem;
+    font-size: 2.3rem;
   }
 
   .language-toggle-wrapper {
-    background-color: peachpuff;
     padding: 1rem;
     font-size: 1.5rem;
   }
 `;
 
 export const MenuItem = styled.div`
-  background-color: pink;
-  padding: 2rem 2.4rem;
-  font-size: 1.8rem;
+  padding: 2rem 2.6rem;
+  font-size: 1.7rem;
 `;
