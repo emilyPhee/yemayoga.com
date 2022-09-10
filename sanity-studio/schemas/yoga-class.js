08598b3f1e3ec8f20b33d61be94793b1caeac8d6
Anthony Park <kunhpark15@gmail.com>
2022-09-10 15:11:38 -0700

    define yoga class schema

---
+++ sanity-studio/schemas/yoga-class.js
@@ -0,0 +1,39 @@
+export default {
+  title: 'Yoga Class',
+  name: 'yogaClass',
+  type: 'document',
+  fields: [
+    {
+      title: 'Name',
+      name: 'name',
+      type: 'string',
+      validation: (Rule) => Rule.required().max(80),
+    },
+    {
+      title: 'Duration in minutes',
+      name: 'duration',
+      type: 'number',
+      validation: (Rule) => Rule.min(1).max(1000),
+    },
+    {
+      title: 'Level',
+      name: 'level',
+      type: 'array',
+      of: [{ type: 'string' }],
+      options: {
+        list: [
+          { title: 'All level', value: 'all level' },
+          { title: 'Beginner', value: 'beginner' },
+          { title: 'Intermediate', value: 'intermediate' },
+          { title: 'Advanced', value: 'advanced' },
+        ],
+      },
+    },
+    {
+      title: 'Monthly Price',
+      name: 'monthlyPrice',
+      type: 'number',
+      validation: (Rule) => Rule.min(0),
+    },
+  ],
+};
