71eacc102bdeba9dafdbeaff45a02235b5ab29ce
Emily <pheepia7788@gmail.com>
2022-09-15 10:55:21 -0700

    add classes component

---
+++ nextjs-client/src/components/YogaClassesList/index.js
@@ -0,0 +1,12 @@
+import { Container } from './style';
+
+const YogaClassesList = () => {
+  return (
+    <Container>
+      <h1>Yoga Classes List</h1>
+      <ul></ul>
+    </Container>
+  );
+};
+
+export default YogaClassesList;
