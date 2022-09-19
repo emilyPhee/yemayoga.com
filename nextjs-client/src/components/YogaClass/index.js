294add8431f377f70d5cb8291ca0be17a31e0c71
Emily <pheepia7788@gmail.com>
2022-09-19 10:49:29 -0700

    commit before change branch

--- nextjs-client/src/components/YogaClass/index.js
+++ nextjs-client/src/components/YogaClass/index.js
@@ -7,16 +7,16 @@ const YogaClass = ({ data }) => {
 
   return (
     <Container>
-      <div className='class-img'>
+      <div className="class-img">
         <Image
-          alt='yoga class'
+          alt="yoga class"
           src={urlFor(image).width(250).height(320).url()}
-          layout='fill'
+          layout="fill"
         />
-        <h4 className='name'>{name}</h4>
+        <h4 className="name">{name}</h4>
       </div>
-      <p className='level'>{level.join(' | ')}</p>
-      {duration ? <p className='duration'>{`${duration} min`}</p> : null}
+      <p className="level">{level.join(' | ')}</p>
+      {duration ? <p className="duration">{`${duration} min`}</p> : null}
     </Container>
   );
 };
