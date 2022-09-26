71c13d255f44884747a0e72837be19545d02e8a0
Anthony Park <kunhpark15@gmail.com>
2022-09-25 23:54:54 -0700

    define review schema

--- sanity-studio/schemas/schema.js
+++ sanity-studio/schemas/schema.js
@@ -4,6 +4,7 @@ import createSchema from 'part:@sanity/base/schema-creator';
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type';
 import yogaClass from './yoga-class';
+import review from './review';
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -14,5 +15,6 @@ export default createSchema({
   types: schemaTypes.concat([
     /* Your types here! */
     yogaClass,
+    review,
   ]),
 });
