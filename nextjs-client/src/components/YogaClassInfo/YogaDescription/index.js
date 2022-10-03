783ee9509a570e7bf5293d931cb797e41243e620
Emily <pheepia7788@gmail.com>
2022-10-03 12:20:14 -0700

    add  yoga description component

---
+++ nextjs-client/src/components/YogaClassInfo/YogaDescription/index.js
@@ -0,0 +1,13 @@
+import { Container } from './style';
+import Image from 'next/image';
+import { urlFor } from 'src/sanity/image-url';
+
+const YogaDescription = () => {
+  return (
+    <div>
+      <h1>Yoga description</h1>
+    </div>
+  );
+};
+
+export default YogaDescription;
