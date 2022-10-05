2766327a84d20a6c54b626e0ed94d9e7c445e389
Emily <pheepia7788@gmail.com>
2022-10-05 11:06:37 -0700

    display data from Sanity and add style

--- nextjs-client/src/components/YogaClassInfo/YogaEtiquette/index.js
+++ nextjs-client/src/components/YogaClassInfo/YogaEtiquette/index.js
@@ -1,10 +1,20 @@
 import { Container } from './style';
 
 const YogaEtiquette = ({ data }) => {
+  const { name, etiquette } = data;
+  console.log(name, etiquette);
   return (
     <Container>
-      <div className="left-box"></div>
-      <div className="right-box"></div>
+      <div className="left-box">
+        <h1 className="title">Etiquette in {name}</h1>
+      </div>
+      <div className="right-box">
+        <ol className="etiquette-list">
+          {etiquette.map((e, index) => (
+            <li key={index}>{e}</li>
+          ))}
+        </ol>
+      </div>
     </Container>
   );
 };
