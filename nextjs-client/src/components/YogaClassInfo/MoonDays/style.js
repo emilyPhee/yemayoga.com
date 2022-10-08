1eada0f781c7a7cba63ed235104cc275e971635a
Emily <pheepia7788@gmail.com>
2022-10-07 17:00:45 -0700

    add tooltip and change style with Style Props

--- nextjs-client/src/components/YogaClassInfo/MoonDays/style.js
+++ nextjs-client/src/components/YogaClassInfo/MoonDays/style.js
@@ -28,10 +28,26 @@ export const Container = styled.section`
     border-bottom: 1px solid #efefef;
   }
 
+  .tooltip-wrapper {
+    width: 100%;
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  .tooltip {
+    font-size: ${theme.sizes.bodyFontsSize};
+    font-family: ${theme.fonts.nunito};
+    text-decoration: underline;
+    color: ${theme.colors.greyText};
+    cursor: pointer;
+    margin-right: 15%;
+    margin-top: 3rem;
+  }
+
   ${theme.mediaQuery.smallScreen} {
     .moonday-list-wrapper {
       grid-template-columns: 1fr 1fr;
-      width: 90%;
+      width: 85%;
     }
   }
 `;
