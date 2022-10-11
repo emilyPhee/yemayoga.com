ee504e92dc7ce66cba6395dee738108539f2569f
Emily <pheepia7788@gmail.com>
2022-10-11 14:05:41 -0700

    remove instructors schema
    include instructor in about schema

--- sanity-studio/schemas/about.js
+++ sanity-studio/schemas/about.js
@@ -32,10 +32,37 @@ export default {
       validation: Rule => Rule.required().max(400),
     },
     {
-      title: 'Instructors',
-      name: 'instructors',
-      type: 'array',
-      of: [{ type: 'instructor' }],
+      title: 'Instructor Name',
+      name: 'instructor_name',
+      type: 'string',
+    },
+    {
+      title: 'Sub Name',
+      name: 'sub',
+      type: 'string',
+      validation: Rule => Rule.optional().max(20),
+    },
+    {
+      title: 'Instructor Introduction (KR)',
+      name: 'introduction_kr',
+      type: 'text',
+      description: 'Please type the introduction in Korean.',
+      validation: Rule => Rule.required().max(1300),
+    },
+    {
+      title: 'Instructor Introduction (EN)',
+      name: 'introduction_en',
+      type: 'text',
+      description: 'Please type the introduction in English.',
+      validation: Rule => Rule.required().max(1300),
+    },
+    {
+      title: 'Instructor Image',
+      name: 'instructor_img',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
     },
   ],
 };
