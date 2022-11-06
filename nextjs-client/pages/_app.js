2d2ee901be68258b0719898060756db024e5df56
Emily <pheepia7788@gmail.com>
2022-11-06 13:40:17 -0800

    add favicon with Favicon Generator

--- nextjs-client/pages/_app.js
+++ nextjs-client/pages/_app.js
@@ -15,7 +15,30 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <title>Yemayoga</title>
-        <link rel="shortcut icon" type="image/svg" href="/images/favicon.svg" />
+
+        {/* Favicon links */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#e1dcd7" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+        {/*  */}
 
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta name="google" content="notranslate"></meta>
