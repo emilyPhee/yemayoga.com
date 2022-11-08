cc92ea917e8ca1bdab9dc9a870f92d4aa23a26e3
Anthony Park <kunhpark15@gmail.com>
2022-11-07 18:16:18 -0800

    use sanity api cdn

--- nextjs-client/src/sanity/client.js
+++ nextjs-client/src/sanity/client.js
@@ -3,7 +3,7 @@ import sanityClient from '@sanity/client';
 const client = sanityClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  useCdn: false,
+  useCdn: true,
   ignoreBrowserTokenWarning: true,
 });
 
